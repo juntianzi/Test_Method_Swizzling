a08de9643f85fe4a63e3b599f98d1c4a8fdd0780
juntianzi <chenfengde9202@163.com>
2015-08-12 14:12:30 +0800

    testSummary
    justTest

--- Test_Method_Swizzling/NSArray+Swizzle.h
+++ Test_Method_Swizzling/NSArray+Swizzle.h
@@ -5,9 +5,25 @@
 //  Created by caf on 15/7/29.
 //  Copyright (c) 2015年 kkkwan. All rights reserved.
 //
-
+/**
+ *  <#Description#>
+ *
+ *  @param Swizzle <#Swizzle description#>
+ *
+ *  @return <#return value description#>
+ */
 #import <Foundation/Foundation.h>
-
+/**
+ *  <#Description#>
+ */
 @interface NSArray (Swizzle)
+/**
+ *  <#Description#>
+ *
+ *  @return <#return value description#>
+ */
 - (id)myLastObject;
+/**
+ *  <#Description#>
+ */
 @end
--- Test_Method_Swizzling/NSArray+Swizzle.m
+++ Test_Method_Swizzling/NSArray+Swizzle.m
@@ -5,10 +5,23 @@
 //  Created by caf on 15/7/29.
 //  Copyright (c) 2015年 kkkwan. All rights reserved.
 //
-
+/**
+ *  <#Description#>
+ *
+ *  @param Swizzle <#Swizzle description#>
+ *
+ *  @return <#return value description#>
+ */
 #import "NSArray+Swizzle.h"
-
+/**
+ *  <#Description#>
+ */
 @implementation NSArray (Swizzle)
+/**
+ *  <#Description#>
+ *
+ *  @return <#return value description#>
+ */
 - (id)myLastObject
 {
     id ret = [self myLastObject];//这不递归了么？别忘记这是我们准备调换IMP的selector，[self myLastObject] 将会执行真的 [self lastObject] 。
